fce9f804d08c40dc86cea5129514e0ccd5f5e19d
phwnganh <phuonganh25102003@gmail.com>
2025-05-30 11:17:32 +0700

    insert message success or error for login

--- src/pages/GuestPage/Login/components/LoginForm.jsx
+++ src/pages/GuestPage/Login/components/LoginForm.jsx
@@ -1,8 +1,8 @@
-import { Button, Form, Input, Space } from "antd";
+import { Button, Form, Input, message, Space } from "antd";
 import { useAuth } from "../../../../context/useAuth";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { DASHBOARD } from "../../../../constants/routes.constants";
+import { DASHBOARD, SIGNUP } from "../../../../constants/routes.constants";
 
 const layout = {
   labelCol: { span: 8 },
@@ -18,17 +18,18 @@ const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const [messageApi, contextHolder] = message.useMessage();
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const onSubmit = async (values) => {
     setLoading(true);
     try {
       const userData = await login(values.email, values.password);
-      alert("Login successfully!");
+      messageApi.success("Login successfully!");
       navigate(DASHBOARD);
       console.log("Login successful, userData:", userData);
     } catch (error) {
-      setError(error.message);
+      messageApi.error(error.message);
     } finally {
       setLoading(false);
     }
@@ -41,61 +42,80 @@ const LoginForm = () => {
     setError(null);
   };
   return (
-    <Form
-      {...layout}
-      form={form}
-      onFinish={onSubmit}
-      className="space-y-6"
-      layout="vertical"
-      initialValues={{ email: "", password: "" }}
-    >
-      <Form.Item
-        name="email"
-        label={<span className="text-gray-700 font-medium">Email</span>}
-        rules={[
-          { required: true, message: "Please enter your email" },
-          { type: "email", message: "Please enter a valid email" },
-        ]}
+    <>
+      {contextHolder}
+      <Form
+        {...layout}
+        form={form}
+        onFinish={onSubmit}
+        className="space-y-6"
+        layout="vertical"
+        initialValues={{ email: "", password: "" }}
       >
-        <Input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your email"
-        />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        label={<span className="text-gray-700 font-medium">Password</span>}
-        rules={[{ required: true, message: "Please enter your password" }]}
-      >
-        <Input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Enter your password"
-        />
-      </Form.Item>
-      {error && (
-        <Form.Item {...tailLayout}>
-          <span style={{ color: "red" }}>{error}</span>
+        <Form.Item
+          name="email"
+          label={<span className="text-gray-700 font-medium">Email</span>}
+          rules={[
+            { required: true, message: "Please enter your email" },
+            { type: "email", message: "Please enter a valid email" },
+          ]}
+        >
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+          />
         </Form.Item>
-      )}
-      <Form.Item {...tailLayout}>
-        <Space>
-          <Button type="primary" htmlType="submit" loading={loading}
-          className="w-full bg-indigo-600 hover:bg-indigo-700 transition-colors duration-200 h-10">
-            {loading ? "Logging in..." : "Login"}
-          </Button>
-          <Button htmlType="button" onClick={onReset} className="ml-2 border-gray-300 hover:border-gray-400 transition-colors duration-200 h-10">
-            Reset
-          </Button>
-        </Space>
-        <div className="text-center mt-4 mr-10" wrapperCol={layout.wrapperCol}>
-          <p>Don't have an account?&nbsp;<span><a href="#" className="text-indigo-600 hover:text-indigo-900 text-sm">Sign up</a></span></p>
-        </div>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          name="password"
+          label={<span className="text-gray-700 font-medium">Password</span>}
+          rules={[{ required: true, message: "Please enter your password" }]}
+        >
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+          />
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Space>
+            <Button
+              type="primary"
+              htmlType="submit"
+              loading={loading}
+              className="w-full bg-indigo-600 hover:bg-indigo-700 transition-colors duration-200 h-10"
+            >
+              {loading ? "Logging in..." : "Login"}
+            </Button>
+            <Button
+              htmlType="button"
+              onClick={onReset}
+              className="ml-2 border-gray-300 hover:border-gray-400 transition-colors duration-200 h-10"
+            >
+              Reset
+            </Button>
+          </Space>
+          <div
+            className="text-center mt-4 mr-10"
+            wrapperCol={layout.wrapperCol}
+          >
+            <p>
+              Don't have an account?&nbsp;
+              <span>
+                <a
+                  href={SIGNUP}
+                  className="text-indigo-600 hover:text-indigo-900 text-sm"
+                >
+                  Sign up
+                </a>
+              </span>
+            </p>
+          </div>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
