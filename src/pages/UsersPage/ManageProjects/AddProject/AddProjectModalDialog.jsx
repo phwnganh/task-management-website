3fae10cfc02212d691afa25fc3591d25f03c7a0d
phwnganh <phuonganh25102003@gmail.com>
2025-06-04 12:27:47 +0700

    done template modal for project detail and project update

---
+++ src/pages/UsersPage/ManageProjects/AddProject/AddProjectModalDialog.jsx
@@ -0,0 +1,8 @@
+import AddProjectForm from "./components/AddProjectForm"
+
+const AddProjectModalDialog = () => {
+      return (<>
+      <AddProjectForm/></>)
+}
+
+export default AddProjectModalDialog
