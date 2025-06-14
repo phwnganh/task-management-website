967f0fd72c26bdd19f4c67ca48098c21958473da
phwnganh <phuonganh25102003@gmail.com>
2025-06-15 00:22:54 +0700

    done template for user list

--- src/pages/AdminPage/AdminDashboard/components/UserOverviewDashboard.jsx
+++ src/pages/AdminPage/AdminDashboard/components/UserOverviewDashboard.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import PostLoginLayout from "../../../../layouts/PostLoginLayout/PostLoginLayout";
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
+import { MANAGE_USER_LIST } from "../../../../constants/routes.constants";
 
 const UserOverviewDashboard = () => {
-      return (
-            <div>
-                  user overview dashboard
+      const navigate = useNavigate()
+  return (
+    <div>
+        <div className="max-w-7xl mx-auto p-4 sm:p-5">
+          <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-4">
+            <div className="flex items-center space-x-4">
+              <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl whitespace-nowrap">
+                User Overview Dashboard
+              </h1>
             </div>
-      );
+            <div className="mt-2 md:mt-0">
+              <Button type="primary" size="large" onClick={() => navigate(MANAGE_USER_LIST)}>
+                View Detail
+              </Button>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
 };
 
 export default UserOverviewDashboard;
