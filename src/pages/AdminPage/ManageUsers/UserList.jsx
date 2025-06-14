9aed4b19632a9a136e5ae44ab2f9ffbba6e27355
phwnganh <phuonganh25102003@gmail.com>
2025-06-15 00:06:57 +0700

    design template for user list admin

--- src/pages/AdminPage/ManageUsers/UserList.jsx
+++ src/pages/AdminPage/ManageUsers/UserList.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import UserListActionTools from "./components/UserListActionTools";
 import UserListTableTools from "./components/UserListTableTools";
 import UserListTable from "./components/UserListTable";
+import PostLoginLayout from "../../../layouts/PostLoginLayout/PostLoginLayout";
 
 const UserList = () => {
   return (
     <div>
-      <UserListActionTools />
-      <UserListTableTools />
-      <UserListTable />
+      <PostLoginLayout>
+        <div className="max-w-7xl mx-auto p-4 sm:p-5">
+          <UserListActionTools />
+          <UserListTableTools />
+          <UserListTable />
+        </div>
+      </PostLoginLayout>
     </div>
   );
 };
--- src/pages/AdminPage/ManageUsers/components/UserListActionTools.jsx
+++ src/pages/AdminPage/ManageUsers/components/UserListActionTools.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UserListActionTools = () => {
       return (
-            <div>
+            <div className='flex flex-col md:flex-row justify-between items-start md:items-center mb-4'>
                   user list action tools
             </div>
       );
