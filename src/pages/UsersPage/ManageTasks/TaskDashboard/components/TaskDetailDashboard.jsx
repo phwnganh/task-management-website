a54c6991f7c93b04d695c0de07b73418711102b4
TranTuan66 <188287043+TranTuan-ops@users.noreply.github.com>
2025-06-16 03:44:01 +0700

    .

--- src/pages/UsersPage/ManageTasks/TaskDashboard/components/TaskDetailDashboard.jsx
+++ src/pages/UsersPage/ManageTasks/TaskDashboard/components/TaskDetailDashboard.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const TaskDetailDashboard = () => {
-      return (
-            <div>
-                  task detail dashboard
-            </div>
-      );
+  return <div>task detail dashboard Tuan</div>;
 };
 
 export default TaskDetailDashboard;
