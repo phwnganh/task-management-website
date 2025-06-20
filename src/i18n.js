be18346bcf1463d23b114997189bbad7489862f9
TranTuan66 <188287043+TranTuan-ops@users.noreply.github.com>
2025-06-20 07:14:17 +0700

    Remove TH

--- src/i18n.js
+++ src/i18n.js
@@ -5,67 +5,56 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import sidebarEN from "./locales/en/SideBar.json";
 import sidebarVI from "./locales/vi/SideBar.json";
 import sidebarJP from "./locales/jp/SideBar.json";
-import sidebarTH from "./locales/th/SideBar.json";
 import sidebarZH from "./locales/zh/SideBar.json";
 import sidebarKO from "./locales/ko/SideBar.json";
 import userdashboardEN from "./locales/en/UserDashboard.json";
 import userdashboardVI from "./locales/vi/UserDashboard.json";
 import userdashboardJP from "./locales/jp/UserDashboard.json";
-import userdashboardTH from "./locales/th/UserDashboard.json";
 import userdashboardZH from "./locales/zh/UserDashboard.json";
 import userdashboardKO from "./locales/ko/UserDashboard.json";
 import headerEN from "./locales/en/Header.json";
 import headerVI from "./locales/vi/Header.json";
 import headerJP from "./locales/jp/Header.json";
-import headerTH from "./locales/th/Header.json";
 import headerZH from "./locales/zh/Header.json";
 import headerKO from "./locales/ko/Header.json";
 import labelEN from "./locales/en/Label.json";
 import labelVI from "./locales/vi/Label.json";
 import labelJP from "./locales/jp/Label.json";
-import labelTH from "./locales/th/Label.json";
 import labelZH from "./locales/zh/Label.json";
 import labelKO from "./locales/ko/Label.json";
 import taskownerEN from "./locales/en/TaskOwner.json";
 import taskownerVI from "./locales/vi/TaskOwner.json";
 import taskownerJP from "./locales/jp/TaskOwner.json";
-import taskownerTH from "./locales/th/TaskOwner.json";
 import taskownerZH from "./locales/zh/TaskOwner.json";
 import taskownerKO from "./locales/ko/TaskOwner.json";
 import taskmemberEN from "./locales/en/TaskMember.json";
 import taskmemberVI from "./locales/vi/TaskMember.json";
 import taskmemberJP from "./locales/jp/TaskMember.json";
-import taskmemberTH from "./locales/th/TaskMember.json";
 import taskmemberZH from "./locales/zh/TaskMember.json";
 import taskmemberKO from "./locales/ko/TaskMember.json";
 import dashboardEN from "./locales/en/Dashboard.json";
 import dashboardVI from "./locales/vi/Dashboard.json";
 import dashboardJP from "./locales/jp/Dashboard.json";
-import dashboardTH from "./locales/th/Dashboard.json";
 import dashboardZH from "./locales/zh/Dashboard.json";
 import dashboardKO from "./locales/ko/Dashboard.json";
 import userinforEN from "./locales/en/UserInfor.json";
 import userinforVI from "./locales/vi/UserInfor.json";
 import userinforJP from "./locales/jp/UserInfor.json";
-import userinforTH from "./locales/th/UserInfor.json";
 import userinforZH from "./locales/zh/UserInfor.json";
 import userinforKO from "./locales/ko/UserInfor.json";
 import changepwEN from "./locales/en/ChangePW.json";
 import changepwVI from "./locales/vi/ChangePW.json";
 import changepwJP from "./locales/jp/ChangePW.json";
-import changepwTH from "./locales/th/ChangePW.json";
 import changepwZH from "./locales/zh/ChangePW.json";
 import changepwKO from "./locales/ko/ChangePW.json";
 import taskcalendarEN from "./locales/en/TaskCalendar.json";
 import taskcalendarVI from "./locales/vi/TaskCalendar.json";
 import taskcalendarJP from "./locales/jp/TaskCalendar.json";
-import taskcalendarTH from "./locales/th/TaskCalendar.json";
 import taskcalendarZH from "./locales/zh/TaskCalendar.json";
 import taskcalendarKO from "./locales/ko/TaskCalendar.json";
 import managerprojectEN from "./locales/en/ManagerProject.json";
 import managerprojectVI from "./locales/vi/ManagerProject.json";
 import managerprojectJP from "./locales/jp/ManagerProject.json";
-import managerprojectTH from "./locales/th/ManagerProject.json";
 import managerprojectZH from "./locales/zh/ManagerProject.json";
 import managerprojectKO from "./locales/ko/ManagerProject.json";
 
@@ -115,19 +104,6 @@ i18n
         changepwuser: changepwJP,
         taskcalendar: taskcalendarJP,
         mp: managerprojectJP,
-      },
-      th: {
-        sidebar: sidebarTH,
-        userdashboard: userdashboardTH,
-        header: headerTH,
-        labellist: labelTH,
-        taskowner: taskownerTH,
-        taskmember: taskmemberTH,
-        dashboard: dashboardTH,
-        userinfor: userinforTH,
-        changepwuser: changepwTH,
-        taskcalendar: taskcalendarTH,
-        mp: managerprojectTH,
       },
       zh: {
         sidebar: sidebarZH,
