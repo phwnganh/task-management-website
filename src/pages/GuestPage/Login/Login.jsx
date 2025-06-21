de5e7256183e531d6ffed4105af4eec085a2e758
TranTuan66 <188287043+TranTuan-ops@users.noreply.github.com>
2025-06-21 15:05:32 +0700

    fix reset

--- src/pages/GuestPage/Login/Login.jsx
+++ src/pages/GuestPage/Login/Login.jsx
@@ -4,7 +4,11 @@ import LoginForm from "./components/LoginForm";
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <PreLoginLayout FormComponent={LoginForm} title={"Login To Your Account"} subtitle={"Welcome back! Please enter your credentials!"}/>
+      <PreLoginLayout
+        FormComponent={LoginForm}
+        title={"Login To Your Account"}
+        subtitle={"Welcome back! Please enter your credentials!"}
+      />
     </div>
   );
 };
