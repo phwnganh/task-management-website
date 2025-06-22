91eb577f4ab17ac956a4bb6d95c739ec1ce4074d
phwnganh <phuonganh25102003@gmail.com>
2025-06-22 23:40:49 +0700

    merge from tuan

--- src/layouts/PostLoginLayout/PostLoginLayout.jsx
+++ src/layouts/PostLoginLayout/PostLoginLayout.jsx
@@ -1,12 +1,12 @@
 import Header from "./Header";
 import SideBar from "./SideBar";
 
-const PostLoginLayout = ({ children, hideSidebar = false }) => {
+const PostLoginLayout = ({ children}) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <div className="flex flex-1">
-        {!hideSidebar && <SideBar />}
+        <SideBar />
         <main className="flex-1 pt-16 lg:pl-72 min-h-[calc(100vh-64px)]">
           {children}
         </main>
