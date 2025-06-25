ef778eabb1641feeea5a7d06717956bae04f2cf9
TranTuan66 <188287043+TranTuan-ops@users.noreply.github.com>
2025-06-26 04:21:42 +0700

    fix error

--- src/layouts/PostLoginLayout/SideBar.jsx
+++ src/layouts/PostLoginLayout/SideBar.jsx
@@ -14,7 +14,7 @@ import { Avatar, Modal } from "antd";
 import { USER } from "../../constants/role.constants";
 import { UserOutlined } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
-import sidebarPic from "../../assets/b1.gif";
+import sidebarPic from "../../assets/a.gif";
 
 const SideBar = () => {
   const { t } = useTranslation("sidebar");
@@ -205,7 +205,7 @@ const SideBar = () => {
             <img
               src={sidebarPic}
               alt="Rocket Animation"
-              className="h-15 object-contain"
+              className="h-25 object-contain"
             />
           </div>
         </nav>
