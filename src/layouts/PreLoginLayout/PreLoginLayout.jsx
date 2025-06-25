7003eda38ba0b58b0a795fd80778f8f719ea98fd
TranTuan66 <188287043+TranTuan-ops@users.noreply.github.com>
2025-06-25 20:31:46 +0700

    oki

--- src/layouts/PreLoginLayout/PreLoginLayout.jsx
+++ src/layouts/PreLoginLayout/PreLoginLayout.jsx
@@ -1,21 +1,11 @@
 import LayoutImage from "../../assets/layouts.jpg";
-import LayoutVideo from "../../assets/layoutsVideo.mp4";
+import LayoutVideo from "../../assets/deMeo.mp4";
 
 const PreLoginLayout = ({ FormComponent, title, subtitle }) => {
   return (
     <div className="flex items-center justify-center min-h-screen px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col lg:flex-row w-full max-w-4xl">
-        <div className="w-full lg:w-1/2 p-8 sm:p-10">
-          <div className="text-center lg:text-left mb-6">
-            <h1 className="text-3xl font-extrabold text-gray-900 mb-2 ml-5">
-              {title}
-            </h1>
-            <h4 className="text-md font-medium text-gray-600 ml-5">
-              {subtitle}
-            </h4>
-          </div>
-          <FormComponent />
-        </div>
+        {/* VIDEO - BÊN TRÁI */}
         <div className="hidden lg:block w-1/2">
           <video
             src={LayoutVideo}
@@ -25,6 +15,20 @@ const PreLoginLayout = ({ FormComponent, title, subtitle }) => {
             playsInline
             className="w-full h-full object-cover"
           />
+        </div>
+
+        {/* FORM - BÊN PHẢI */}
+        <div className="w-full lg:w-1/2 p-8 sm:p-10">
+          <div className="text-center lg:text-left mb-6">
+            <h1 className="text-3xl font-extrabold text-gray-900 mb-2 ml-5">
+              {title}
+            </h1>
+            <h4 className="text-md font-medium text-gray-600 ml-5">
+              {subtitle}
+            </h4>
+          </div>
+
+          <FormComponent />
         </div>
       </div>
     </div>
