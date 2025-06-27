c6a28e7b7df543b74fda8b3e2609863341f80d5a
TranTuan66 <188287043+TranTuan-ops@users.noreply.github.com>
2025-06-27 17:33:57 +0700

    ..

--- src/pages/GuestPage/Login/Login.jsx
+++ src/pages/GuestPage/Login/Login.jsx
@@ -1,20 +1,10 @@
 import PreLoginLayout from "../../../layouts/PreLoginLayout/PreLoginLayout";
 import LoginForm from "./components/LoginForm";
 import MapOSMOut from "../../../mapOSM/components/MapOSMOut";
-import sidebarPic from "../../../assets/b.gif";
 
 const Login = () => {
   return (
     <div className="relative min-h-screen flex flex-col items-center justify-center">
-      {/* 🐱 Ảnh động ở góc trái */}
-      <div className="fixed top-4 left-4 z-50">
-        <img
-          src={sidebarPic}
-          alt="Funny Cat"
-          className="w-24 h-24 object-contain animate-bounce"
-        />
-      </div>
-
       {/* 📝 Form đăng nhập */}
       <PreLoginLayout
         FormComponent={LoginForm}
